68b36987b96216eab74289998077e4be6ed216fc
xxx <1106829005@qq.com>
2019-06-06 18:00:46 +0800

    no message

--- app/src/main/java/com/lldj/tc/MainActivity2.java
+++ app/src/main/java/com/lldj/tc/MainActivity2.java
@@ -93,7 +93,7 @@ public class MainActivity2 extends BaseActivity implements HandlerInter.HandleMs
 //                String s = "{\"error\":0,\"status\":\"success\",\"results\":[{\"currentCity\":\"青岛\",\"index\":[{\"title\":\"穿衣\",\"zs\":\"较冷\",\"tipt\":\"穿衣指数\",\"des\":\"建议着厚外套加毛衣等服装。年老体弱者宜着大衣、呢外套加羊毛衫。\"},{\"title\":\"紫外线强度\",\"zs\":\"最弱\",\"tipt\":\"紫外线强度指数\",\"des\":\"属弱紫外线辐射天气，无需特别防护。若长期在户外，建议涂擦SPF在8-12之间的防晒护肤品。\"}]}]}";
 //                Gson gson = new Gson();
 //                //把JSON数据转化为对象
-                JsonBean jsonBean = gson.fromJson(s, JsonBean.class);
+//                JsonBean jsonBean = gson.fromJson(s, JsonBean.class);
 //
 //                Log.w("-----ssssss", jsonBean.getResults().get(0).getCurrentCity());
 
