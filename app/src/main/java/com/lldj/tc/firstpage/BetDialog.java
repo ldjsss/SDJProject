604158b702b3945005b5a1b84e1b86701a388a5d
xxx <1106829005@qq.com>
2019-06-25 20:18:25 +0800

    no message

--- app/src/main/java/com/lldj/tc/firstpage/BetDialog.java
+++ app/src/main/java/com/lldj/tc/firstpage/BetDialog.java
@@ -2,7 +2,9 @@ package com.lldj.tc.firstpage;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Build;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -14,6 +16,8 @@ import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.StyleRes;
 import com.lldj.tc.R;
@@ -28,6 +32,8 @@ import com.lldj.tc.toolslibrary.event.Observer;
 import com.lldj.tc.toolslibrary.handler.HandlerInter;
 import com.lldj.tc.toolslibrary.util.AppUtils;
 import com.lldj.tc.toolslibrary.util.Clog;
+import com.lldj.tc.toolslibrary.view.ToastUtils;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -38,7 +44,7 @@ import butterknife.OnClick;
 
 import static android.view.ViewGroup.FOCUS_BEFORE_DESCENDANTS;
 
-public class BetDialog<dismiss> extends Dialog {
+public class BetDialog extends Dialog {
 
     @BindView(R.id.gamebettotalcount)
     TextView gamebettotalcount;
@@ -48,27 +54,27 @@ public class BetDialog<dismiss> extends Dialog {
     private ExpandableListView expandableListView;
 
     //注意，字符数组不要写成{{"A1,A2,A3,A4"}, {"B1,B2,B3,B4，B5"}, {"C1,C2,C3,C4"}}
-    private String[][] childs = {{"A1"}, {"A1"}, {"A1"}, {"A1"}, {"A1"}};
+    private String[][] childs = {{"A1"}, {"A1"}, {"A1"}, {"A1"}, {"A1"}, {"A1"}, {"A1"}, {"A1"}, {"A1"}, {"A1"}, {"A1"}, {"A1"}, {"A1"}};
     private Map editTexts = new HashMap();
     private Observer<ObData> observer;
     private List<ObData> groups = new ArrayList<>();
     private MyExpandableListView _myExpandableListView;
 
+    int screenHeight;
+    int maxHeight = 0;
+    int _oneHeight = 0;
+
     public BetDialog(@NonNull Context context, @StyleRes int themeResId) {
         super(context, themeResId);
 
-        //2、设置布局
+        screenHeight = AppUtils.getDisplayMetrics(getContext()).heightPixels;
+
         View view = View.inflate(context, R.layout.dialog_bet_layout, null);
         setContentView(view);
-
         ButterKnife.bind(this, view);
-
         Window window = this.getWindow();
-        //设置弹出位置
         window.setGravity(Gravity.BOTTOM);
-        //设置弹出动画
         window.setWindowAnimations(R.style.main_menu_animStyle);
-        //设置对话框大小
         window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
         WindowManager.LayoutParams layoutParams = window.getAttributes();
@@ -96,21 +102,6 @@ public class BetDialog<dismiss> extends Dialog {
                     }
                 }
 
-                if(count > 0){
-                    expandableListView.getLayoutParams().height = _height + 60;
-                }
-            }
-        });
-        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
-            @Override
-            public void onGroupCollapse(int groupPosition) {
-                int count = expandableListView.getExpandableListAdapter().getGroupCount();
-                boolean GroupExpanded = false;
-                for(int j = 0; j < count; j++){
-                    if(expandableListView.isGroupExpanded(j))GroupExpanded = true;
-                }
-
-                if(!GroupExpanded) expandableListView.getLayoutParams().height = _height;
             }
         });
 
@@ -118,10 +109,12 @@ public class BetDialog<dismiss> extends Dialog {
             @Override
             public void onUpdate(Observable<ObData> observable, ObData data) {
                 if (data.getKey().equalsIgnoreCase(EventType.BETLISTADD)) {
+                    if(groups.size()>childs.length){
+                        ToastUtils.show_middle_pic(getContext(), R.mipmap.cancle_icon, getContext().getResources().getString(R.string.maxSelect), ToastUtils.LENGTH_SHORT);
+                        return;
+                    }
                     groups.add(0, data);
-
                     update();
-
                     if(expandableListView != null) expandableListView.expandGroup(0);
                 }
             }
@@ -135,6 +128,15 @@ public class BetDialog<dismiss> extends Dialog {
     private void update(){
         _myExpandableListView.notifyDataSetChanged();
         gamebettotalcount.setText(groups.size() + "");
+
+        if(maxHeight <= 0) maxHeight = screenHeight - findViewById(R.id.viewlayout).getLayoutParams().height - findViewById(R.id.bettitlelayout).getLayoutParams().height;
+        if(_oneHeight <= 0){
+            View countview = LayoutInflater.from(getContext()).inflate(R.layout.item_group, null);
+            _oneHeight = countview.findViewById(R.id.groupLayout).getLayoutParams().height;
+        }
+        int newHeight =(int)( _oneHeight*(groups.size()+1.2));
+        if(newHeight > maxHeight)newHeight = maxHeight;
+        if(expandableListView != null)expandableListView.getLayoutParams().height = newHeight;
 
     }
 
@@ -326,6 +328,24 @@ public class BetDialog<dismiss> extends Dialog {
             }
             notifyDataSetChanged();
         }
+    }
+
+    private void fullScreenImmersive(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            view.setSystemUiVisibility(uiOptions);
+        }
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        fullScreenImmersive(getWindow().getDecorView());
     }
 
 }
