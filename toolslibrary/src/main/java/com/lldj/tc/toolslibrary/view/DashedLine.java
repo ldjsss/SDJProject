a603f0e81bb7569bf51443d7ae4e47238ed28c78
xxx <1106829005@qq.com>
2019-06-27 17:23:30 +0800

    no message

--- toolslibrary/src/main/java/com/lldj/tc/toolslibrary/view/DashedLine.java
+++ toolslibrary/src/main/java/com/lldj/tc/toolslibrary/view/DashedLine.java
@@ -1,4 +1,40 @@
 package com.lldj.tc.toolslibrary.view;
 
-public class DashedLine {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathEffect;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DashedLine extends View {
+    int mColor = Color.BLUE;
+    public DashedLine(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DashedLine(Context context, AttributeSet attrs, int mColor) {
+        super(context, attrs);
+        this.mColor = mColor;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.reset();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(1);
+        paint.setColor(mColor);
+        PathEffect pathEffect = new DashPathEffect(new float[]{5,5,5,5},1);
+        paint.setPathEffect(pathEffect);
+        Path path = new Path();
+        path.moveTo(0,10);
+        path.lineTo(480,10);
+        canvas.drawPath(path, paint);
+    }
 }
