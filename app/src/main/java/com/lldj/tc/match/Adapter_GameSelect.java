48d780dab8093dd3a770056e222776cb950603d9
xxx <1106829005@qq.com>
2019-07-02 20:21:18 +0800

    no message

--- app/src/main/java/com/lldj/tc/match/Adapter_GameSelect.java
+++ app/src/main/java/com/lldj/tc/match/Adapter_GameSelect.java
@@ -3,6 +3,7 @@ package com.lldj.tc.match;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,15 +23,18 @@ import com.lldj.tc.httpMgr.beans.FormatModel.ResultsModel;
 import com.lldj.tc.httpMgr.beans.FormatModel.matchModel.BetModel;
 import com.lldj.tc.toolslibrary.http.HttpTool;
 import com.lldj.tc.toolslibrary.util.AppUtils;
+import com.lldj.tc.toolslibrary.util.Clog;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Adapter_GameSelect extends RecyclerView.Adapter<Adapter_GameSelect.MyViewHolder> {
     private Context context;
     private Adapter_GameSelect.MyViewHolder mHolder  = null;
     private List<ResultsModel> datas = new ArrayList<>();
+    private int selectID = 0;
 
     public Adapter_GameSelect(Context context, List<ResultsModel> datas){
         this.context = context;
@@ -42,7 +47,7 @@ public class Adapter_GameSelect extends RecyclerView.Adapter<Adapter_GameSelect.
 
         View v = View.inflate(context, R.layout.gameselectcell,null);
         Adapter_GameSelect.MyViewHolder holder = new Adapter_GameSelect.MyViewHolder(v);
-        holder.setIsRecyclable(false);
+//        holder.setIsRecyclable(false);
         return holder;
     }
 
@@ -78,33 +83,63 @@ public class Adapter_GameSelect extends RecyclerView.Adapter<Adapter_GameSelect.
         public void bottomCommon() {
             int pos = getAdapterPosition();
             for (int i = 0; i < 3; i++) {
-                layout[i].removeAllViews();
-                LayoutInflater inflater = LayoutInflater.from(context);
-                View view = inflater.inflate(R.layout.onegamecell, null);
-                view.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
-                view.getLayoutParams().width = oneWidth;
-                view.getLayoutParams().height = oneWidth;
-                layout[i].addView(view);
-
                 int _len = pos*3+i;
+                ResultsModel _data = null;
+
                 if(datas.size() >= _len){
-                    ResultsModel _data = datas.get(_len);
-                    ((TextView)view.findViewById(R.id.gamename)).setText(_data.getName());
-
-                    HttpTool.getBitmapUrl(_data.getLogo(), new HttpTool.bmpListener() {
-                        @Override
-                        public void onFinish(Bitmap bitmap) {
-                            if (bitmap != null) ((ImageView)view.findViewById(R.id.imggameicon)).setImageBitmap(bitmap);
-                        }
-                    });
-
+                    _data = datas.get(_len);
                     layout[i].setVisibility(View.VISIBLE);
                 }
                 else{
                     layout[i].setVisibility(View.GONE);
+                    return;
                 }
+
+                int _id = _data.getId();
+                View view = layout[i].findViewById(R.id.game_view);
+
+                if(view == null) {
+                    LayoutInflater inflater = LayoutInflater.from(context);
+                    view = inflater.inflate(R.layout.onegamecell, null);
+                    view.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                    view.getLayoutParams().width = oneWidth;
+                    view.getLayoutParams().height = oneWidth;
+                    layout[i].addView(view);
+                    view.setId(R.id.game_view);
+
+                    view.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            selectID = (int) v.getTag();
+                            notifyDataSetChanged();
+                        }
+                    });
+                }
+
+                Object _tag = view.getTag();
+                if(_tag == null || (int)view.getTag() != _id){
+                    ImageView img = (ImageView) view.findViewById(R.id.imggameicon);
+                    HttpTool.getBitmapUrl(_data.getLogo(), new HttpTool.bmpListener() {
+                        @Override
+                        public void onFinish(Bitmap bitmap) {
+                            if (bitmap != null) img.setImageBitmap(bitmap);
+                        }
+                    });
+                }
+                ((TextView)view.findViewById(R.id.gamename)).setText(_data.getName());
+
+                setViewColor(view, (selectID == _id));
+
+                view.setTag(_id);
             }
 
+        }
+
+        private void setViewColor(View v, boolean select){
+            if(select)
+                v.findViewById(R.id.selectline).setBackgroundColor(Color.parseColor("#85E1E2"));
+            else
+                v.findViewById(R.id.selectline).setBackgroundColor(Color.parseColor("#383b4a"));
         }
     }
 }
